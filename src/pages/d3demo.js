551fcf8b3c9a83f090fcc5d512cb1867950d9a5e
Thomas Rueckstiess <thomas@rueckstiess.net>
2015-09-01 12:53:09 +1000

    more details on d3 demo page.

--- src/pages/d3demo.js
+++ src/pages/d3demo.js
@@ -12,7 +12,19 @@ export default React.createClass({
       <div>
         <h1>React with d3 demo</h1>
         <p>this is a hot-loadable react component that renders d3 inside. Go ahead, change the
-        code in <code>src/d3/bar.js</code>, hit save, and see what happens.</p>
+        code, hit save, and see what happens.</p>
+
+        <h4>Example: Change Color</h4>
+        <p>For example, in <code>src/d3/bar.js</code>, replace the color palette with
+        <code>category20b()</code> at the end of this line</p>
+        <pre>
+        const color = d3.scale.category20c()
+        </pre>
+
+        <h4>Example: Manipulate Data</h4>
+        <p>Or, in <code>src/components/random-data.js</code>, add or remove some of the <code>data</code>
+        lines.</p>
+
         <RandomDataGenerator>
           <BarChart className="BarChart" width={400} height={300}/>
         </RandomDataGenerator>
